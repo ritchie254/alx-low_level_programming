f98bbebc962452807036722cb6dd29d9caa9a1fd
ritchie254 <richardoduor012@gmail.com>
2023-08-10 07:22:11 -0700

    using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - entry point into the program
+  * Return: 0 if success
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
