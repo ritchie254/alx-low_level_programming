7658ca9ce1529864c650bd92ac8c01cb8a6316c7
ritchie254 <richardoduor012@gmail.com>
2023-08-11 04:02:49 -0700

    for loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * Return: 0 when successful
+  */
+int main(void)
+{
+	/*declaration of variables*/
+	int j;
+
+	/* now using a for loop */
+	for (j = 48; j <= 57; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	/* return value */
+	return (0);
+}
