f7db4956aa769257acfe5480adc4286b513e79aa
ritchie254 <richardoduor012@gmail.com>
2023-08-11 07:35:41 -0700

    combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return - 0 if succes
+  */
+int main(void)
+{
+	/*delcalration */
+	int a = '0';
+	int b, c;
+
+	/* intialization */
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			while (c <= '9')
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == '7' && b =='8' && c == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (0);
+}
