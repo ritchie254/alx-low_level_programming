6f75c928326ca027a5bcbabc4693ded79550004a
ritchie254 <richardoduor012@gmail.com>
2023-08-11 06:22:43 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return: 0 success
+  */
+int main(void)
+{
+	/* decalration of variables */
+	int i, j;
+
+	/*intializiation */
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			if (i >= j)
+			{
+				continue;
+				j++;
+			}
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
