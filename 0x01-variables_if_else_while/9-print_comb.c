3e46423647a2b2dc90b6e06f6a169981b502c05c
ritchie254 <richardoduor012@gmail.com>
2023-08-11 04:45:47 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - entry point to program
+  *Return: 0 success
+  */
+int main(void)
+{
+	/* declaration */
+	int i;
+
+	/* initializiation */
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	/* return value */
+	return (0);
+}
