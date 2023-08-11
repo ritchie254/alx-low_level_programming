1b8d2709488504ba4e56bfb9058883439aa371e2
ritchie254 <richardoduor012@gmail.com>
2023-08-11 03:54:32 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *Return: 0 when successfull
+  */
+int main(void)
+{
+	/* declaration of varaibles */
+	int i = 48;
+
+	/* using while loop */
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	/* return value */
+	return (0);
+}
