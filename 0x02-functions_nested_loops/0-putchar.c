4f110946ac466e89202fb283889f6adb40029e28
ritchie254 <richardoduor012@gmail.com>
2023-08-15 03:06:33 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *main - entry point to the program
+  *Return: 0 when successful
+  */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	/* return statement */
+	return (0);
+}
