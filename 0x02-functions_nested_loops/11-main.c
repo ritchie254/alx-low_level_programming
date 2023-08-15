d71f1644e533e31c00f162fdcfaf819e8ce03a4c
ritchie254 <richardoduor012@gmail.com>
2023-08-15 07:35:32 -0700

    98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *main - checks the code
+  *
+  *Return: 0 success
+  */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *print_to_98 - print values with 98 as the last value
+  *@n: integer values to begin at
+  *
+  */
+
+void print_to_98(int n)
+{
+	int i;
+	char m = ',', k = ' ';
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i == 98)
+			continue;
+		printf("%c%c", m, k);
+	}
+	printf("\n");
+}
