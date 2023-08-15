55349d69fbd48bcd771543297f333043b913c82e
ritchie254 <richardoduor012@gmail.com>
2023-08-15 05:54:36 -0700

    absolute of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,14 @@ int print_last_digit(int i)
 	int last_digit;
 
 	last_digit = i % 10;
-	return (last_digit);
+	if (last_digit < 0)
+	{
+		_putchar(-last_digit + 48);
+		return (-last_digit);
+	}
+	else
+	{
+		_putchar(last_digit + 48);
+		return(last_digit);
+	}
 }
