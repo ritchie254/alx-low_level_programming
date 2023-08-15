610d2b055801edf6f74de9cf608ed7c83f9c69d7
ritchie254 <richardoduor012@gmail.com>
2023-08-15 07:48:21 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,5 +19,15 @@ void print_to_98(int n)
 			continue;
 		printf("%c%c", m, k);
 	}
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+		if (i == 98)
+			continue;
+		printf("%c%c", m, k);
+		}
+	}
 	printf("\n");
 }
