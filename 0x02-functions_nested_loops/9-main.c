b8a96a439857ff76fe46718b6d1b1b3f70e52df0
ritchie254 <richardoduor012@gmail.com>
2023-08-15 09:33:56 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *main - check the code
+  *
+  *Return: 0 success
+  */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+  * times_table - prints multiplication table
+  *
+  *Return: 0 success
+  */
+
+void times_table(void)
+{
+	int x = 0, y, z;
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
