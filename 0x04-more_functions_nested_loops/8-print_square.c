0d25b615c964e3a477aa9127bdef056203798031
ritchie254 <richardoduor012@gmail.com>
2023-08-17 06:16:15 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,6 +10,10 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -18,5 +22,4 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
