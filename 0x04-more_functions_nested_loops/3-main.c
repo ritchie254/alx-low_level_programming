9845ba50393d0cc1c6da4f18bbbdcce7678beb04
ritchie254 <richardoduor012@gmail.com>
2023-08-17 02:55:03 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *Return: 0
+  */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_numbers - print numbers 0 to 9
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
