278fb510b463cc41a820af75fa05509e976f0f65
ritchie254 <richardoduor012@gmail.com>
2023-08-17 06:03:31 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *print_square - square shape
+  *@size: integer input
+  *
+  */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
