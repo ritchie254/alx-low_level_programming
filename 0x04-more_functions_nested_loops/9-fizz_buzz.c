82e2b9fae683c0b66e0b5bee4b76231eded2f800
ritchie254 <richardoduor012@gmail.com>
2023-08-17 06:52:38 -0700

    fizbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *Return: 0
+  */
+
+int main(void)
+{
+	/* fizzbuzz game */
+	int i;
+	char a[25] = "Fizz";
+	char b[25] = "Buzz";
+	char c[25] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s ", c);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ",b);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s ", a);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
