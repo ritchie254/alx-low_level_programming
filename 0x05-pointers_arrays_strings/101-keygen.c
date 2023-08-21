49cef983f87a8e48ebd377d3c6f4124e5b6f45b7
ritchie254 <richardoduor012@gmail.com>
2023-08-21 07:09:29 -0700

    crack

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - entry point
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i = 0, j = 0, k = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (j < 2772)
+	{
+		i = rand() % 128;
+		if ((j + i) > 2772)
+			break;
+		j += i;
+		k++;
+		printf("%c", i);
+	}
+	printf("%c\n", (2772 - j));
+	return (0);
+}
