6fcbc48b5af0f901cf14e7f0496b68aa2661995a
ritchie254 <richardoduor012@gmail.com>
2023-08-21 04:54:49 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**rev_string - reverse string
+  *@s: string
+  *
+  */
+
+void rev_string(char *s)
+{
+	int i = 0, j;
+	char c, d;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i -= 1;
+	for (j = 0; j < i; j++)
+	{
+		c = s[j];
+		d = s[i];
+		s[j] = d;
+		s[i] = c;
+		i--;
+	}
+}
