b5eb8abeba5ed535593448bff481a3933ecc7839
ritchie254 <richardoduor012@gmail.com>
2023-08-24 06:36:37 -0700

    string  to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,23 +2,20 @@
 
 /**
   *string_toupper - upper
-  *@value: string
+  *@str: string
   *Return: string
   */
 
-char *string_toupper(char *value)
+char *string_toupper(char *str)
 {
-	int size = sizeof(value) - 1;
 	int i;
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (value[i] >= 'a' && value[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			value[i] = value[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
-	value[size + 1] = '\n';
-	return (value);
+	return (str);
 }
-
