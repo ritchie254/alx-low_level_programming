2d77466a115c19a62315eec6ae759da5b7a8ed68
ritchie254 <richardoduor012@gmail.com>
2023-08-28 03:46:05 -0700

    string search

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *_strpbrk - search a string
+  *@s: s
+  *@accept: accept
+  *Return: string match
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 
 #endif
