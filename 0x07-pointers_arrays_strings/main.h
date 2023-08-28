89bf2c81c430046903a7170fbd0e30960f8872ac
ritchie254 <richardoduor012@gmail.com>
2023-08-28 03:59:13 -0700

    length of string prefix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
