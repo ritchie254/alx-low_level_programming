e9a4bd55fe89d9635f1e2ddbd2c25e80f9784001
ritchie254 <richardoduor012@gmail.com>
2023-08-28 03:21:19 -0700

    substing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+char *_strstr(char *haystack, char *needle);
 
 #endif
