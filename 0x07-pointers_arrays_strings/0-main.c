b2e2c6e77d0fba3ff8ff82fa1a5c94ddb2843573
ritchie254 <richardoduor012@gmail.com>
2023-08-28 01:22:39 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *simple_print_buffer - print buffer
+  *@buffer: address of memory to print
+  *@size: size of memory
+  */
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+  *main - entry
+  *Return: 0
+  */
+
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
