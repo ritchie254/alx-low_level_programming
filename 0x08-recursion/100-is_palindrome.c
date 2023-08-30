83bf58174363047486b8098835a349feaacee0c3
ritchie254 <richardoduor012@gmail.com>
2023-08-30 12:08:25 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+int _strlen_recursion(char *s);
+int _scan(char *c, char *d);
+
+/**
+  *is_palindrome - check if string is palindrome
+  *@s: string
+  *
+  *Return: 1 if true else 0
+  */
+
+int is_palindrome(char *s)
+{
+	int i = _strlen_recursion(s);
+	char *c = s;
+	char *d = s + (i - 1);
+
+	if (i == 0)
+		return (1);
+	return (_scan(c, d));
+}
+
+/**
+  *_strlen_recursion - length of string
+  *@s: string
+  *
+  *Return: length of string
+  */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (0);
+	else
+	{
+		i = _strlen_recursion(s + 1);
+		i++;
+	}
+	return (i);
+}
+
+/**
+  *_scan - scans string
+  *@c: character
+  *@d: charcter
+  *
+  *Return: integer
+  */
+
+int _scan(char *c, char *d)
+{
+	if (*c != *d)
+		return (0);
+	else if (c >= d)
+		return (1);
+	else
+		return (_scan(c + 1, d - 1));
+}
