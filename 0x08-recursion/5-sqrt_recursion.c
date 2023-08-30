fde7a07dc84c5a225a62ab21785e584009b29039
ritchie254 <richardoduor012@gmail.com>
2023-08-30 02:06:32 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,18 +10,14 @@
 
 int _sqrt_check(int i, int j)
 {
-	if (j * *2 == i)
-	{
-		return (j);
-	}
-	else if (j * *2 > i)
-	{
+	int s;
+
+	s = j * j;
+	if (s > i)
 		return (-1);
-	}
-	else
-	{
-		return (_sqrt_check(i, j + 1));
-	}
+	if (s == i)
+		return (j);
+	return (_sqrt_check(i, j + 1));
 }
 
 /**
@@ -33,9 +29,5 @@ int _sqrt_check(int i, int j)
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return (_sqrt_check(n, 0));
+	return (_sqrt_check(n, 1));
 }
