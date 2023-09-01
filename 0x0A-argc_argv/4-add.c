0f2a34bf5f5e77b123fc17a1f92fe9dbb1967606
ritchie254 <richardoduor012@gmail.com>
2023-09-01 05:16:42 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *@argc: arguement count
+  *@argv: arguement array or vector
+  *
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
