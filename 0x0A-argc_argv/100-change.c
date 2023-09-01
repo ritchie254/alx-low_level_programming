60b27d4f776f15043ea1acfe4f3af2f85940fafa
ritchie254 <richardoduor012@gmail.com>
2023-09-01 05:50:05 -0700

    Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *@argc: arguement count
+  *@argv: arguement array or vector
+  *
+  *Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int cents, count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			cents -= 25;
+		}
+		else if (cents >= 10)
+		{
+			cents -= 10;
+		}
+		else if (cents >= 5)
+		{
+			cents -= 5;
+		}
+		else if (cents >= 2)
+		{
+			cents -= 2;
+		}
+		else if (cents >= 1)
+		{
+			cents -= 1;
+		}
+		count++;
+	}
+	printf("%d\n", count);
+	return (0);
+}
