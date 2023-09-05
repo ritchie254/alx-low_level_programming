84e78244bb5603e12e4885453bab80a0353c83e2
ritchie254 <richardoduor012@gmail.com>
2023-09-05 08:48:54 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,26 +2,46 @@
 #include <stdlib.h>
 
 /**
+  * argstostr - prints aurguements passed in a function
+  *@ac: aurguement counts
+  *@av: array
   *
+  *Return: pointer to character
   */
 
 char *argstostr(int ac, char **av)
 {
 	char *ptr;
-	int i;
+	int i, j, n = 0, length = 0;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	ptr = (char *)malloc(sizeof(char) * ac);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			length++;
+		}
+	}
+	length += ac;
+	ptr = malloc(sizeof(char) * (length + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < ac; i++)
 	{
-		ptr[i] = av[i];
+		for (j = 0; av[i][j]; j++)
+		{
+			ptr[n] = av[i][j];
+			n++;
+		}
+		if (ptr[n] == '\0')
+		{
+			ptr[n++] = '\n';
+		}
 	}
 	return (ptr);
 }
