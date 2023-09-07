bfe6dd6eb33b7e2a765ea438de1440701f92586b
ritchie254 <richardoduor012@gmail.com>
2023-09-07 09:16:36 -0700

    string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+  *string_nconcat - concertinates two stings
+  *@s1: first string
+  *@s2: second string
+  *@n: number of bytes to be copied
+  *
+  *Return: pointer to character string
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *string;
+	size_t s1_length, s2_length, i, j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	s1_length = 0;
+	while (s1[s1_length] != '\0')
+	{
+		s1_length++;
+	}
+	s2_length = 0;
+	while (s2[s2_length] != '\0')
+	{
+		s2_length++;
+	}
+	string = malloc(sizeof(char) * (s1_length + n) + 1);
+	if (string == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < s1_length; i++)
+	{
+		string[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		string[s1_length] = s2[j];
+		s1_length++;
+	}
+	return (string);
+}
