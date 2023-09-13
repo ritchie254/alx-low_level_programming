a9a445c4ce68f3c0b96ca2c23e223739758ca0d8
ritchie254 <richardoduor012@gmail.com>
2023-09-13 05:47:43 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+  * get_op_func - a pointer to the function that
+  *corresponds to the op as a parm
+  * @s: operator passed as argument
+  * Return: Always 0 for sucess
+  */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*(ops[i].op) == *s && s[0] != '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+  *op_add - adds numbers
+  *@a: interger
+  *@b: interger
+  *Return: results
+  */
+
+int op_add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
+
+/**
+  *op_sub - subtract numbers
+  *@a: interger
+  *@b: interger
+  *Return: results
+  */
+
+int op_sub(int a, int b)
+{
+	int result;
+
+	result = a - b;
+	return (result);
+}
+
+/**
+  *op_mul - multiply numbers
+  *@a: interger
+  *@b: interger
+  *Return: results
+  */
+
+int op_mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
+
+/**
+  *op_div - divides numbers
+  *@a: interger
+  *@b: interger
+  *Return: results
+  */
+
+int op_div(int a, int b)
+{
+	int result;
+
+	result = a / b;
+	return (result);
+}
+
+/**
+  * op_mod - returns the modulus value of two numbers input
+  *@a: integer
+  *@b: interger
+  *Return: result
+  */
+
+int op_mod(int a, int b)
+{
+	int result;
+
+	result = a % b;
+	return (result);
+}
