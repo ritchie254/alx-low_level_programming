f0bbdbd83a692caf2276d112d65c11f51a020a1c
ritchie254 <richardoduor012@gmail.com>
2023-09-13 04:31:35 -0700

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - a function that returns the index of a number
+  *@array: an array
+  *@size: size of the array
+  *@cmp: pointer to a function
+  *
+  *Return: -1 and exits
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	/*
+	*if (array == NULL || cmp == NULL)
+	*	return;
+	*/
+	if (size <= 0)
+		return (-1);
+	for (i = 0;  i < size; i++)
+	{
+		if (cmp(*(array + i)))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
