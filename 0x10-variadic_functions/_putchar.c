3588df0beddf91d5c87296c45c47764f337c2731
ritchie254 <richardoduor012@gmail.com>
2023-09-14 08:37:21 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/_putchar.c
+++ 0x10-variadic_functions/_putchar.c
@@ -3,9 +3,9 @@
 /**
  * _putchar - writes character c to stdout
  * @c: char to be printed
- * Return: On success 1 
+ * Return: On success 1
  */
 int _putchar(char c)
 {
-	 return (write(1, &c, 1));           
+	return (write(1, &c, 1));
 }
