57ac32cc97bf0efa9cf98522ef2624506a34e49b
ritchie254 <richardoduor012@gmail.com>
2023-09-21 11:59:08 -0700

    hare nad tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * printFirst - prints a string
+  *
+  */
+
+void printFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
