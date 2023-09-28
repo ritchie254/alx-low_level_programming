b23ab8a1880dccea8f4d3057e03b134b51f6eb14
ritchie254 <richardoduor012@gmail.com>
2023-09-28 07:45:54 -0700

    binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+int set_bit(unsigned long int *n, unsigned int index);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
