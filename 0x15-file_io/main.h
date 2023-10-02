4f90ad6e3c25d4c907ff97ac93aeb405b44c0664
ritchie254 <richardoduor012@gmail.com>
2023-10-02 04:54:45 -0700

    creating a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,9 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void error_98(int f0, char *buffer, char *argv);
+void error_99(int f0, char *buffer, char *argv);
+void error_100(int f0, char *buffer);
 
 #endif /* header file */
