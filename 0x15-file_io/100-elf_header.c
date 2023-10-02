97da73c9a267b83c136862933e9ff3f7275b6b70
ritchie254 <richardoduor012@gmail.com>
2023-10-02 05:07:14 -0700

    Tread lightly, she is near

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
-* main - main
-* Return: always 0
-*/
+  * display_elf_header - a program that displays the information contained in the ELF header
+  *@header: header
+  */
 
-int main(void)
+void display_elf_header(const Elf64_Ehdr *header)
 {
-
-	return (0);
+	printf("Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\n");
+	printf("Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+	printf("Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little-endian" : "2's complement, big-endian");
+	printf("Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI:                            %s\n", (header->e_ident[EI_OSABI] == ELFOSABI_SYSV) ? "UNIX - System V" : "Other");
+	printf("ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type:                              %s\n", (header->e_type == ET_EXEC) ? "EXEC (Executable file)" : (header->e_type == ET_DYN) ? "DYN (Shared object file)" : (header->e_type == ET_REL) ? "REL (Relocatable file)" : "Other");
+	printf("Entry point address:                0x%lx\n", header->e_entry);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <elf.h>
 
+void display_elf_header(const Elf64_Ehdr *header);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
